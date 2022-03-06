4873f88e41f25dad0c373a24917bcbd1e4940df8
downey <dutch.x@outlook.com>
2022-03-06 11:22:12 +0100

    added: calender, edited: routes

--- website/routes/views.py
+++ website/routes/views.py
@@ -9,3 +9,8 @@ def home():
     bungalows = Bungalows().query.all()
     return render_template('bungalows.html', bungalows = bungalows)
  
+@views.route('bungalows/bungalow/<int:bungalow_id>')
+def bungalow(bungalow_id):
+    bungalow = Bungalows().query.get_or_404(bungalow_id)
+    return render_template('bungalow.html', bungalow = bungalow)
+    
