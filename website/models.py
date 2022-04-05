fd5616b4a088f7a24cf1fa9d91a3b3d95ff681c2
Mentalist <33149862+Mentalist@users.noreply.github.com>
2022-04-05 20:52:14 +0200

    Init Auth

--- website/models.py
+++ website/models.py
@@ -1,31 +1,44 @@
 from . import db
+import uuid
+from flask_login import UserMixin
+from . import db
 
-
-class Bungalows(db.Model):
+class Bungalow(db.Model):
     __tablename__ = 'bungalows'
-    id = db.Column(db.Integer(), primary_key=True, nullable=False, unique=True)
-    name = db.Column(db.String(50), nullable=False, unique=True)
-    Description = db.Column(db.Text(), nullable=True)
-    num_people = db.Column(db.Integer(), nullable=False)
-    image = db.Column(db.String(100), nullable=False, unique=True)
+    uuid = db.Column(db.String, primary_key=True, nullable=False, unique=True)
+    title = db.Column(db.String(50), nullable=False, unique=True)
+    description = db.Column(db.Text(), nullable=True)
+    min_p = db.Column(db.Integer(), nullable=False)
+    max_p = db.Column(db.Integer(), nullable=False)
+    img_b64 = db.Column(db.String(100), nullable=False, unique=True)
     price = db.Column(db.Float(), nullable=False)
     reservations = db.relationship('Reservations', backref='bungalows')
+    created_at = db.Column(db.Date(), nullable=False)
+    updated_at = db.Column(db.Date(), nullable=False)
+    deleted_at = db.Column(db.Date(), nullable=False)
 
 
-class Users(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer(), primary_key=True, nullable=False, unique=True)
+    uuid = db.Column(db.String, primary_key=True, nullable=False, unique=True)
     username = db.Column(db.String(32), nullable=False, unique=True)
+    email = db.Column(db.String(100), unique=True)
     password = db.Column(db.Text(), nullable=False)
     role = db.Column(db.String(16), nullable=False, default='user')
     reservations = db.relationship('Reservations', backref='users')
+    created_at = db.Column(db.Date(), nullable=False)
+    updated_at = db.Column(db.Date(), nullable=False)
+    deleted_at = db.Column(db.Date(), nullable=False)
 
 
-class Reservations(db.Model):
+class Reservation(db.Model):
     __tablename__ = 'reservations'
-    id = db.Column(db.Integer(), primary_key=True, nullable=False, unique=True)
-    bunglalows = db.Column(db.Integer(), db.ForeignKey('bungalows.id'))
+    uuid = db.Column(db.String, primary_key=True, nullable=False, unique=True)
+    bungalow = db.Column(db.Integer(), db.ForeignKey('bungalows.id'))
     user = db.Column(db.Integer(), db.ForeignKey('users.id'))
-    beg_date = db.Column(db.Date(), nullable=False)
+    begin_date = db.Column(db.Date(), nullable=False)
     end_date = db.Column(db.Date(), nullable=False)
     total = db.Column(db.Float(), nullable=False)
+    created_at = db.Column(db.Date(), nullable=False)
+    updated_at = db.Column(db.Date(), nullable=False)
+    deleted_at = db.Column(db.Date(), nullable=False)
